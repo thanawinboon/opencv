56b73f8d2ab6fa3b5d3dec180c1a946a83cd75fb
muidstim <tim.gir@mahidol.edu>
2018-10-10 17:46:05 -0900

    Update testCircle.py

--- testCircle.py
+++ testCircle.py
@@ -1,15 +1,44 @@
+#imports computer vision version 2
 import cv2
-#import imutils
+
+#import the numercial processing library for python, call it np for short
 import numpy as np
 
+imageName = 'circles.jpg'
+
 try:
-    img = cv2.imread('coins.jpg',0)
+    #load an image from the disk
+    img = cv2.imread(imageName,0)
+    cimg = cv2.imread(imageName)
+    
+    #blur the image, so that similar colors get combined
+    #https://docs.opencv.org/3.1.0/d4/d13/tutorial_py_filtering.html
     img = cv2.medianBlur(img,5)
-    ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
+    
+    cv2.imshow('detected circles',img)
+    
+    cv2.waitKey(0)
 
-    cimg = cv2.cvtColor(thresh1,cv2.COLOR_GRAY2BGR)
-
-    circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20, param1=20,param2=40,minRadius=80,maxRadius=100)
+    """
+    Detect circles in the image
+    https://docs.opencv.org/3.1.0/da/d53/tutorial_py_houghcircles.html
+    
+    minDist: Minimum distance between the center (x, y) coordinates of detected circles.
+    If the minDist is too small, multiple circles in the same neighborhood as the original
+    may be (falsely) detected. If the minDist is too large, then some circles may not be
+    detected at all.
+    
+    param1 will set the sensitivity; how strong the edges of the circles need to be.
+    Too high and it won't detect anything, too low and it will find too much clutter.
+    
+    param2 will set how many edge points it needs to find to declare that it's found
+    a circle. Again, too high will detect nothing, too low will declare anything to be
+    a circle. The ideal value of param 2 will be related to the circumference of the circles.
+    
+    If you have an idea what size circles you are looking for, then it would be best to
+    set min_radius and max_radius accordingly. Otherwise, it will return anything circular of any size.
+    """
+    circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,minDist=30, param1=10,param2=22,minRadius=10,maxRadius=100)
 
     circles = np.uint16(np.around(circles))
 
@@ -25,12 +54,6 @@ try:
     print(count)
     cv2.imshow('detected circles',cimg)
     
-    px = cimg[100,100]
-    
-#    ball = img[550:340, 330:390]
- #   img[550:333, 100:160] = ball
-    print(px)
-    
     cv2.waitKey(0)
     
     cv2.imwrite('foundcircle.png',cimg)
@@ -38,3 +61,4 @@ try:
 
 finally:
     cv2.destroyAllWindows()
+
