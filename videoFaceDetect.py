fbc1a58b851bb62951e20f6287978c20c3d8bfd0
muidstim <tim.gir@mahidol.edu>
2018-10-31 18:22:25 -0900

    Update videoFaceDetect.py

--- videoFaceDetect.py
+++ videoFaceDetect.py
@@ -18,28 +18,32 @@ try:
      
     # capture frames from the camera
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-            # grab the raw NumPy array representing the image
-            img = frame.array
-            
-            #convert to gray scale for the face detect algorithm
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # grab the raw NumPy array representing the image
+        img = frame.array
 
-            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        #convert to gray scale for the face detect algorithm
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-            for (x, y, w, h) in faces:
-                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-     
-            # show the frame
-            cv2.imshow("Frame", img)
+        #use the haar cascade to detect faces
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        #loop through all the faces found and draw rectangles around them
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            #roi_gray = gray[y:y + h, x:x + w]
+            #roi_color = img[y:y + h, x:x + w]
             
-            key = cv2.waitKey(1) & 0xFF
-     
-            # clear the stream in preparation for the next frame
-            rawCapture.truncate(0)
-     
-            # if the `q` key was pressed, break from the loop
-            if key == ord("q"):
-                break
+        # show the frame
+        cv2.imshow("Frame", img)
+        
+        key = cv2.waitKey(1) & 0xFF
+ 
+        # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+ 
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
 	    
 finally:
     cv2.destroyAllWindows()
