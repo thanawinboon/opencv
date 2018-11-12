3e64f4dda83c1d5b739e0f5549801441acd1a5b4
muidstim <tim.gir@mahidol.edu>
2018-11-12 11:07:05 +0700

    Update face_recognition.py

--- face_recognition.py
+++ face_recognition.py
@@ -8,10 +8,10 @@ cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath);
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-#iniciate id counter
+# id counter
 id = 0
 
-# names related to ids: example ==> Marcelo: id=1,  etc
+# names related to ids: example ==> Marcelo: id=1,  etc.
 names = ['None', 'Marcelo', 'Paula', 'Ilza', 'Z', 'W'] 
 
 # Initialize and start realtime video capture
@@ -20,11 +20,11 @@ cam.set(3, 640) # set video widht
 cam.set(4, 480) # set video height
 
 # Define min window size to be recognized as a face
-minW = 0.1*cam.get(3)
-minH = 0.1*cam.get(4)
+minW = 0.1 * cam.get(3)
+minH = 0.1 * cam.get(4)
 
 while True:
-    ret, img =cam.read()
+    ret, img = cam.read()
     img = cv2.flip(img, -1) # Flip vertically
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     
@@ -35,9 +35,10 @@ while True:
         minSize = (int(minW), int(minH)),
        )
     
-    for(x,y,w,h) in faces:
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+    for(x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        
+        id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
         
         # Check if confidence is less them 100 ==> "0" is perfect match 
         if (confidence < 100):
@@ -47,15 +48,17 @@ while True:
             id = "unknown"
             confidence = "  {0}%".format(round(100 - confidence))
         
-        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
-        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+        cv2.putText(img, str(id), (x + 5,y - 5), font, 1, (255, 255, 255), 2)
+        cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)  
     
     cv2.imshow('camera',img) 
+    
     k = cv2.waitKey(10) & 0xff # Press 'ESC' for exiting video
     if k == 27:
         break
 
 # Do a bit of cleanup
 print("\n [INFO] Exiting Program and cleanup stuff")
+
 cam.release()
 cv2.destroyAllWindows()
